186bc0979688f748d6fe1d3b13480aa243f68bd3
khangHNISC <59554204+khangHNISC@users.noreply.github.com>
2022-05-20 10:13:33 +0700

    bit wise

---
+++ overview/src/test/java/org/example/BitwiseOpsTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by khangld5 on May 20, 2022
+ */
+class BitwiseOpsTest {
+    @Test
+    void not() {
+        int i = 0B1010;
+        assertEquals(i, 10);
+        // 2 complement = -10 -> flip bit = -10 - 1
+        assertEquals(~i, -11);
+    }
+
+    @Test
+    void leftShift() {
+        int i = 0b1000000;
+        assertEquals(i, 64);
+        assertEquals(i << 2, 256);
+
+        byte a = 64;
+        //0b1111111 = 127 max byte
+        a = (byte) (a << 2);
+        assertEquals(a, 0);
+    }
+
+    @Test
+    void rightShift() {
+        //shift -1 1 always result -1
+        assertEquals(-1 >> 1, -1);
+        //divide by 2 and discard remainder
+        assertEquals(35 >> 2, 8);
+    }
+}
