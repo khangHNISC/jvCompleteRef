cc9f5563f1ec66977a024535d227bb00f0a4305f
khangHNISC <59554204+khangHNISC@users.noreply.github.com>
2022-05-26 17:14:28 +0700

    inheritance module

---
+++ inheritance/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * Created by khangld5 on $MONTH_NAME_SHORT $DAY, $YEAR
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
